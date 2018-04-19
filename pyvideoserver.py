bf7e297cb20888520443d7db57fc033bad54e786
Christian Parker <cp@cjparker.us>
2018-04-18 20:07:31 -0600

    video not freezing

--- pyvideoserver.py
+++ pyvideoserver.py
@@ -13,11 +13,13 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self._writeheaders()
 
-        DataChunkSize = 10000
+        DataChunkSize = 1024
 
-        command = '(echo "--video boundary--"; raspivid --rotation 90 -w 1024 -h 576 -fps 30 -pf baseline -n -t 0 -o -;) | gst-launch-1.0 -e -q fdsrc fd=0 ! video/x-h264,width=1024,height=576,framerate=30/1,stream-format=byte-stream ! h264parse ! mp4mux streamable=true fragment-duration=10 presentation-time=true ! filesink location=/dev/stdout'
+#        command = 'echo "--video boundary--"; gst-launch-1.0 rpicamsrc bitrate=1000000 ! video/x-h264,width=1280,height=720,framerate=15/1,profile=high ! h264parse ! mp4mux streamable=true fragment-duration=10 presentation-time=true ! filesink location=/dev/stdout'
+#WORKS        command = '(echo "--vidXXXXXeo boundary--"; raspivid -v --intra 5 -fps 30 -b 2000000 --rotation 90 -w 1024 -h 576 -pf baseline -n -t 0 -o -;) | gst-launch-1.0 -e -q fdsrc fd=0 ! video/x-h264,width=1024,height=576,framerate=30/1,stream-format=byte-stream ! h264parse ! mp4mux streamable=true fragment-duration=10 presentation-time=true ! filesink location=/dev/stdout'
+        command = 'gst-launch-1.0 -e -q rpicamsrc ! video/x-h264,width=1024,height=576,framerate=30/1,stream-format=byte-stream ! h264parse ! mp4mux streamable=true fragment-duration=10 presentation-time=true ! filesink location=/dev/stdout'
         print("running command: %s" % (command, ))
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=-1, shell=True)
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1024*4, shell=True)
 
         print("starting polling loop.")
         while(p.poll() is None):
